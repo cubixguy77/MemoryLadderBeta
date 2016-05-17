a3976a1639ecabd438e8edde2fef8b9acd9456bd
cubixguy77 <ras12@albion.edu>
2016-05-17 18:21:25 -0400

    Minor refactoring.

--- app/src/main/java/speednumbers/mastersofmemory/com/memoryladder_speednumbers/ChallengeCard.java
+++ app/src/main/java/speednumbers/mastersofmemory/com/memoryladder_speednumbers/ChallengeCard.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,7 +12,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ChallengeCard extends CardView implements IChallengeCard.View {
@@ -23,7 +21,7 @@ public class ChallengeCard extends CardView implements IChallengeCard.View {
     @BindView(R.id.memorizationTimerContainer) RelativeLayout memorizationTimerContainer;
     @BindView(R.id.recallTimerContainer) RelativeLayout recallTimerContainer;
     @BindView(R.id.playButton) Button playButton;
-    @BindView(R.id.downButton) ImageView downButton;
+    @BindView(R.id.expandContractButton) ImageView expandContractButton;
     @BindView(R.id.challengeText) TextView challengeText;
 
     private boolean open = false;
@@ -36,15 +34,13 @@ public class ChallengeCard extends CardView implements IChallengeCard.View {
         super(context, attrs);
     }
 
-    public ChallengeCard(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+    public ChallengeCard(Context context, AttributeSet attrs, int defStyle) { super(context, attrs, defStyle); }
 
-    @OnClick(R.id.downButton) void onArrowClicked() {
+    @OnClick(R.id.expandContractButton) void onArrowClicked() {
         if (!open) {
             //CardView.LayoutParams layoutParams = (CardView.LayoutParams) challengeCard.getLayoutParams();
             //challengeCard.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-            downButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_up));
+            expandContractButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_up));
             digitGroupingContainer.setVisibility(View.VISIBLE);
             memorizationTimerContainer.setVisibility(View.VISIBLE);
             recallTimerContainer.setVisibility(View.VISIBLE);
@@ -52,7 +48,7 @@ public class ChallengeCard extends CardView implements IChallengeCard.View {
             open = true;
         }
         else {
-            downButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_down));
+            expandContractButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_down));
             digitGroupingContainer.setVisibility(View.GONE);
             memorizationTimerContainer.setVisibility(View.GONE);
             recallTimerContainer.setVisibility(View.GONE);
@@ -72,11 +68,11 @@ public class ChallengeCard extends CardView implements IChallengeCard.View {
 
     @Override
     public void expand() {
-
+        expandContractButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_down));
     }
 
     @Override
     public void contract() {
-
+        expandContractButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_arrow_up));
     }
 }
