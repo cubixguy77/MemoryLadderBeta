2591dc51179105aa9152b7dc0e6b148f5858fcc6
Rob Sessions <rsessions@plex.com>
2016-05-19 17:00:32 -0400

    Add callback from challenges to start game play.

--- app/src/main/java/speednumbers/mastersofmemory/com/presentation/ChallengeListActivity.java
+++ app/src/main/java/speednumbers/mastersofmemory/com/presentation/ChallengeListActivity.java
@@ -13,7 +13,7 @@ import speednumbers.mastersofmemory.com.domain.repository.IRepository;
 import speednumbers.mastersofmemory.com.storage.DatabaseHelper;
 import speednumbers.mastersofmemory.com.storage.Repository;
 
-public class ChallengeListActivity extends AppCompatActivity {
+public class ChallengeListActivity extends AppCompatActivity implements IChallengeSelectionListener {
     @BindView(R.id.ChallengeListContainer) LinearLayout root;
 
     @Override
@@ -28,7 +28,7 @@ public class ChallengeListActivity extends AppCompatActivity {
     {
         Repository repo = new Repository(DatabaseHelper.getInstance(this));
 
-        int gameKey = 1; /* This will be passed in the intent bundle from the games screen eventually */
+        int gameKey = 1; /* TODO Get this value from the intent bundle passed in from previous activity */
 
         repo.getChallengeList(gameKey, new IRepository.GetChallengesCallback() {
             @Override
@@ -39,23 +39,10 @@ public class ChallengeListActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
-
-        /*
-        ChallengeCard card = new ChallengeCard(this);
-        ChallengeCardDataModel model = new ChallengeCardDataModel(15);
-        card.setDataModel(model);
-        root.addView(card, 0);
-
-        ChallengeCard card2 = new ChallengeCard(this);
-        ChallengeCardDataModel model2 = new ChallengeCardDataModel(20);
-        card2.setDataModel(model2);
-        root.addView(card2, 1);
-
-        ChallengeCard card3 = new ChallengeCard(this);
-        ChallengeCardDataModel model3 = new ChallengeCardDataModel(30);
-        card3.setDataModel(model3);
-        root.addView(card3, 2);
-        */
+    @Override
+    public void onChallengeSelected(long challengeKey) {
+        /* TODO Navigate to game play */
     }
 }
---
+++ app/src/main/java/speednumbers/mastersofmemory/com/presentation/IChallengeSelectionListener.java
@@ -0,0 +1,5 @@
+package speednumbers.mastersofmemory.com.presentation;
+
+public interface IChallengeSelectionListener {
+    void onChallengeSelected(long challengeKey);
+}
