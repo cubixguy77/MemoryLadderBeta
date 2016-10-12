5f259fe6145c48fd85b622480a51f88789a221d6
Rob - Laptop <ras12@albion.edu>
2016-10-11 23:44:40 -0400

    Implements Review Cell styling.

--- app/src/main/java/review/ReviewCell.java
+++ app/src/main/java/review/ReviewCell.java
@@ -1,6 +1,11 @@
 package review;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StrikethroughSpan;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.AbsListView;
@@ -20,11 +25,51 @@ public class ReviewCell extends TextView {
         boolean correct = memoryString.equals(recallString);
 
         if (correct) {
-            setText(memoryString + System.getProperty("line.separator"));
+            SimpleSpanBuilder ssb = new SimpleSpanBuilder();
+            ssb.append(recallString,new ForegroundColorSpan(Color.GREEN));
+            ssb.append(System.getProperty("line.separator"));
+            ssb.append(memoryString);
+            setText(ssb.build());
         }
         else {
-            String s = memoryString + System.getProperty("line.separator") + recallString;
-            setText(s);
+            /*
+             * -1: Empty
+             *  0: Wrong
+             *  1: Right
+             */
+            int[] result = new int[memoryString.length()];
+
+            for (int i=0; i<memoryString.length(); i++) {
+                if (i < recallString.length()) // Recall character present
+                    result[i] = memoryString.charAt(i) == recallString.charAt(i) ? 1 : 0;
+                else
+                    result[i] = -1;
+            }
+
+            SimpleSpanBuilder ssb = new SimpleSpanBuilder();
+
+            for (int i=0; i<memoryString.length(); i++) {
+                switch (result[i]) {
+                    case -1: ssb.append("—",new ForegroundColorSpan(Color.RED)); break;
+                    case  0: ssb.append(Character.toString(recallString.charAt(i)),new ForegroundColorSpan(Color.RED), new StrikethroughSpan()); break;
+                    case  1: ssb.append(Character.toString(recallString.charAt(i)),new ForegroundColorSpan(Color.GREEN)); break;
+                }
+            }
+
+            ssb.append(System.getProperty("line.separator"));
+            ssb.append(memoryString);
+
+            /*
+            for (int i=0; i<memoryString.length(); i++) {
+                switch (result[i]) {
+                    case -1: ssb.append(Character.toString(memoryString.charAt(i))); break;
+                    case  0: ssb.append(Character.toString(memoryString.charAt(i))); break;
+                    case  1: ssb.append("\u0020"); break;
+                }
+            }
+            */
+
+            setText(ssb.build());
         }
     }
 }
