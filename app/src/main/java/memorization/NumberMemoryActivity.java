357eb967af6e4cd72fc3cd57ba95d2692105a577
Rob - Laptop <ras12@albion.edu>
2016-10-15 23:50:20 -0400

    Makes number memorization full screen.

--- app/src/main/java/memorization/NumberMemoryActivity.java
+++ app/src/main/java/memorization/NumberMemoryActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import javax.inject.Inject;
 
@@ -40,6 +41,8 @@ public class NumberMemoryActivity extends BaseActivityChallenge implements GameS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_memory_numbers);
         ButterKnife.bind(this);
         this.challengeKey = getIntent().getLongExtra("ChallengeKey", -1);
