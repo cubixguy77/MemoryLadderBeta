f0afbd261db5eb86e45ead5963a945f9e8ac5fa1
Rob - Laptop <ras12@albion.edu>
2017-03-18 22:50:45 -0400

    Adds a "Rate This App" option to the home screen's menu.

--- app/src/main/java/selectChallenge/viewChallengeList/ChallengeListActivity.java
+++ app/src/main/java/selectChallenge/viewChallengeList/ChallengeListActivity.java
@@ -1,8 +1,11 @@
 package selectChallenge.viewChallengeList;
 
+import android.annotation.TargetApi;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
@@ -106,6 +109,10 @@ public class ChallengeListActivity extends BaseActivity implements IChallengeSel
             case R.id.action_send_feedback:
                 getUserFeedback();
                 return true;
+
+            case R.id.action_rate_app:
+                rateApp();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -156,6 +163,22 @@ public class ChallengeListActivity extends BaseActivity implements IChallengeSel
         });
 
         alert.show();
+    }
+
+    private void rateApp() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        }
+        else {
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        }
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+        }
     }
 
     private void getUserFeedback() {
